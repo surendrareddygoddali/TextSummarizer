347b1b66905b9b00960a85caff2186ab880cc041
Surendra Reddy Goddali <surendrareddy.goddali@outlook.com>
2023-04-17 16:41:11 -0500

    updated tensorflow module to version 2 and fixed tensorflow 1 bugs

--- attention_decoder.py
+++ attention_decoder.py
@@ -49,8 +49,8 @@ def attention_decoder(decoder_inputs, initial_state, encoder_states, enc_padding
     coverage: Coverage vector on the last step computed. None if use_coverage=False.
   """
   with variable_scope.variable_scope("attention_decoder") as scope:
-    batch_size = encoder_states.get_shape()[0].value # if this line fails, it's because the batch size isn't defined
-    attn_size = encoder_states.get_shape()[2].value # if this line fails, it's because the attention length isn't defined
+    batch_size = encoder_states.get_shape()[0]# .value # if this line fails, it's because the batch size isn't defined
+    attn_size = encoder_states.get_shape()[2]# .value # if this line fails, it's because the attention length isn't defined
 
     # Reshape encoder_states (need to insert a dim)
     encoder_states = tf.expand_dims(encoder_states, axis=2) # now is shape (batch_size, attn_len, 1, attn_size)
